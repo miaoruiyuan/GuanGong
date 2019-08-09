e17488588b6aff7a93e31a36c669da6dcba1603a
miaoruiyuan <miao201110@gmail.com>
2019-08-09 15:27:23 +0800

    Initial commit

---
+++ Example/GuanGong/GGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GGAppDelegate.h
+//  GuanGong
+//
+//  Created by miaoruiyuan on 08/09/2019.
+//  Copyright (c) 2019 miaoruiyuan. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/GuanGong/GGViewController.h
@@ -0,0 +1,13 @@
+//
+//  GGViewController.h
+//  GuanGong
+//
+//  Created by miaoruiyuan on 08/09/2019.
+//  Copyright (c) 2019 miaoruiyuan. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GGViewController : UIViewController
+
+@end
