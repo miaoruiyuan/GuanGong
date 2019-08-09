f2e1687b2fad497d919017b004552bea2c49d083
miaoruiyuan <miao201110@gmail.com>
2019-08-09 10:31:07 +0800

    Initial commit

---
+++ Example/GuanGong/GGViewController.m
@@ -0,0 +1,29 @@
+//
+//  GGViewController.m
+//  GuanGong
+//
+//  Created by miaoruiyuan on 08/09/2019.
+//  Copyright (c) 2019 miaoruiyuan. All rights reserved.
+//
+
+#import "GGViewController.h"
+
+@interface GGViewController ()
+
+@end
+
+@implementation GGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
